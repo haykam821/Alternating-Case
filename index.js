8d421a63a9ae6ddfe4085688b1f044713c60b653
haykam821 <haykam821@users.noreply.github.com>
2018-01-18 14:29:37 -0500

    Basic!

---
+++ index.js
@@ -0,0 +1,9 @@
+exports = function (string) {
+  let characters = string.toLowerCase().split("");
+  let length = characters.length;
+  
+  for (let item = 0; item < length; item += 2) {
+    characters[item] = characters[item].toUpperCase();
+  }
+  return characters.join("");
+};
