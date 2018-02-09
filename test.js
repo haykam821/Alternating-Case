e9bb44ffc2fb73cd973abe1fea8b4d957913431d
haykam821 <haykam821@users.noreply.github.com>
2018-02-08 20:29:33 -0500

    Make some tests

--- test.js
+++ test.js
@@ -1,2 +1,27 @@
 const chai = require('chai');
 const assert = chai.assert;
+
+const altcase = require("./index.js");
+
+const normalTestText = "Tests";
+const normalTest = altCase(normalTestText);
+
+const complexTestText = "A slightly more complex test!";
+const complexTest = altCase(complexTestText);
+
+describe("the function", function() {
+  it("should start with an uppercase letter", function() {
+    assert.equal(normalTest[0], normalTest[0].toUpperCase());
+  });
+  
+  it("should return a string", function() {
+    assert.typeOf(normalTest, "string");
+  });
+  
+  it("should return same length as input", function() {
+    assert.lengthOf(normalTest, normalTestText.length);
+    assert.lengthOf(complexTest, complexTestText.length);
+  });
+  
+  it("should 
+});
