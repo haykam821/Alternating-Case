44d3d01d1bcd70b25d675d9499997c76b36ac1b0
haykam821 <haykam821@users.noreply.github.com>
2018-04-06 23:37:32 -0400

    ESLint magic!

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-module.exports = function (string) {
-  let characters = string.toLowerCase().split("");
-  let length = characters.length;
-  
-  for (let item = 0; item < length; item += 2) {
-    characters[item] = characters[item].toUpperCase();
-  }
-  return characters.join("");
+module.exports = function(string) {
+	const characters = string.toLowerCase().split("");
+	const length = characters.length;
+
+	for (let item = 0; item < length; item += 2) {
+		characters[item] = characters[item].toUpperCase();
+	}
+	return characters.join("");
 };
