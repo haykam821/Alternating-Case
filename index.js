8c48da50be1d7c4f0ac39f8a49a03c2b6ae0485a
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-10 18:08:27 -0400

    Automatically fix things through ESLint

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = (input, capsOnOdds = true) => {
-  const chars = input.split("");
-  return chars.map((value, index) => {
-    if (capsOnOdds) {
-      return index % 2 ? value.toLowerCase() : value.toUpperCase();
-    } else {
-      return index % 2 ? value.toUpperCase() : value.toLowerCase();
-    }
-  }).join("");
-}
+	const chars = input.split("");
+	return chars.map((value, index) => {
+		if (capsOnOdds) {
+			return index % 2 ? value.toLowerCase() : value.toUpperCase();
+		} else {
+			return index % 2 ? value.toUpperCase() : value.toLowerCase();
+		}
+	}).join("");
+};
