20da40c13de67c76c298da506c0bb2c2842b6b1c
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-17 10:39:47 -0400

    Allow overriding toUpperCase and toLowerCase

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
  * Converts every second character in a string to uppercase.
  * @param {string} input The string to convert to alternating case.
  * @param {boolean} capsOnOdds If true, the capitalization is zero-based.
+ * @param {function} upperCase The function to run to transform to uppercase.
+ * @param {function} lowerCase The function to run to transform to lowercase.
  * @returns {string} An alternating-cased string.
  */
-function altCase(input, capsOnOdds = true) {
+function altCase(input, capsOnOdds = true, upperCase = String().toUpperCase, lowerCase = String().toLowerCase()) {
 	const chars = input.split("");
 	return chars.map((value, index) => {
 		if (capsOnOdds) {
-			return index % 2 ? value.toLowerCase() : value.toUpperCase();
+			return index % 2 ? lowerCase.apply(value) : upperCase.apply(value);
 		} else {
-			return index % 2 ? value.toUpperCase() : value.toLowerCase();
+			return index % 2 ? upperCase.apply(value) : lowerCase.apply(value);
 		}
 	}).join("");
 }
