0c2a060197522e9c3900c4de52343fc0a33b1c91
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-01-10 09:15:06 -0500

    Add tests for the capsOnOdds parameter

--- test.js
+++ test.js
@@ -24,4 +24,15 @@ describe("the function", function() {
 		assert.lengthOf(normalTest, normalTestText.length);
 		assert.lengthOf(complexTest, complexTestText.length);
 	});
+
+	describe("capsOnOdds parameter", function() {
+		it("should default the capsOnOdds parameter to true", function() {
+			const text = "should we capitalize odd characters?";
+			assert.strictEqual(altCase(text), altCase(text, true));
+		});
+
+		it("should swap case when the capsOnOdds parameter is false", function() {
+			assert.strictEqual(altCase("Capitals on evens", false), "cApItAlS On eVeNs");
+		});
+	});
 });
